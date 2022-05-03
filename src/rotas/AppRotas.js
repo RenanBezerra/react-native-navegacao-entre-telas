bcd8d686de231af9be4abf12b515bd579092bc82
Renan Bezerra Gomes <renanbg90@gmail.com>
2022-05-02 21:07:22 -0300

    4.3
    Adicionando Icones

--- src/rotas/AppRotas.js
+++ src/rotas/AppRotas.js
@@ -6,11 +6,26 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ProdutorRotas from './ProdutorRotas';
 import MelhoresProdutoresRotas from './MelhoresProdutoresRotas';
 
+import Coracao from '../assets/coracao.svg'
+import Home from '../assets/home.svg'
+
 const Tab = createBottomTabNavigator();
 
 export default function AppRota(){
     return <NavigationContainer>
-    <Tab.Navigator screenOptions={{ headerShown: false}}>
+    <Tab.Navigator screenOptions={ ({ route }) => ({
+      headerShown: false,
+      tabBarIcon:({ color }) => {
+        let Icon = Home; 
+
+        if(route.name === 'MelhoresProdutores'){
+          Icon = Coracao;
+        }
+        return <Icon color={color}/>
+      },
+      tabBarActiveTintColor:'#2A9F85',
+      tabBarInactiveTintColor: '#C7C7C7'  
+      })}>
       <Tab.Screen name='Home' component={ProdutorRotas}/>
       <Tab.Screen name='MelhoresProdutores' component={MelhoresProdutoresRotas}/>
     </Tab.Navigator>
